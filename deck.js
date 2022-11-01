29818920957dbadbbafe8459cfebddd510064615
automatedfuture <createfutureautomations@gmail.com>
2022-11-01 19:36:52 +1300

    Formatting, more functions

--- deck.js
+++ deck.js
@@ -1,8 +1,74 @@
-function getCards(amount) {
-    if (amount == 1) {
-        return "Ace of Spades"
-    }
-    return Array(52)
+const ranks = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+];
+
+const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
 
-module.exports = {getCards}
+function getRandomCard() {
+    return [
+        ranks[getRandomInt(ranks.length)],
+        suits[getRandomInt(suits.length)],
+    ];
+}
+
+function convertHandToString(hand) {
+    handToString = [];
+    for (let i = 0; i < hand.length; i++) {
+        handToString.push(hand[i][0] + " of " + hand[i][1]);
+    }
+    return handToString;
+}
+
+function getCards(amount) {
+    if (amount == 1) {
+        return "Ace of Spades";
+    }
+    if (amount > 52) {
+        console.log("Amount exceeds the number of possible cards (52)");
+        return [];
+    }
+
+    hand = [];
+
+    while (true) {
+        if (hand.length == amount) {
+            break;
+        }
+        random_card = getRandomCard();
+        if (hand.includes(random_card)) {
+            continue;
+        } else {
+            hand.push(random_card);
+        }
+    }
+
+    return hand;
+}
+
+function checkUniqueHand(hand) {
+    const unique = Array.from(new Set(hand));
+
+    if (hand.length === unique.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = { getCards, checkUniqueHand };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,40 @@
-const deck = require('../deck') // this is the line to add
+const deck = require("../deck"); // this is the line to add
 
-test('test setup working', () => {
-    expect(true).toBeTruthy()
-})
+test("test setup working", () => {
+    expect(true).toBeTruthy();
+});
 
-test('gets all the cards', () => {
-    const amount = 52
-    const expected = 52
-    const hand = deck.getCards(amount)
-    const actual = hand.length
-    expect(actual).toBe(expected)
-  })
+test("returns a card", () => {
+    const amount = 1;
+    const expected = "Ace of Spades";
+    const actual = deck.getCards(amount);
+    expect(actual).toBe(expected);
+});
+
+test("gets all the cards", () => {
+    const amount = 52;
+    const expected = 52;
+    const hand = deck.getCards(amount);
+    const checkUnique = deck.checkUniqueHand(hand);
+    const actual = hand.length;
+    expect(actual).toBe(expected);
+    expect(checkUnique).toBe(true);
+});
+
+test("gets 5 cards", () => {
+    const amount = 5;
+    const expected = 5;
+    const hand = deck.getCards(amount);
+    const checkUnique = deck.checkUniqueHand(hand);
+    const actual = hand.length;
+    expect(actual).toBe(expected);
+    expect(checkUnique).toBe(true);
+});
+
+test("no two cards are the same", () => {
+    // your turn
+    const amount = 52;
+    const hand = deck.getCards(amount);
+    const checkUnique = deck.checkUniqueHand(hand);
+    expect(checkUnique).toBe(true);
+});
